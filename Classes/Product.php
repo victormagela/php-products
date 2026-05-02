bfe66eca3c5fd1ff8ca3dd2e9aa7ee9b80930e6e
Victor Magela <victormateusamato@outlook.com>
2026-05-02 14:32:10 -0300

    Create the result page that receives the form and processes it

--- Classes/Product.php
+++ Classes/Product.php
@@ -1,31 +1,31 @@
 <?php
 
-    class Product {
-        private string $name;
-        private float $unit_price;
-        private int $quantity;
+class Product {
+    private string $name;
+    private float $unit_price;
+    private int $quantity;
 
-        public function __construct(string $name, float $unit_price, int $quantity)
-        {
-            $this->name = $name;
-            $this->unit_price = $unit_price;
-            $this->quantity = $quantity;
-
-        }
-
-        public function getName() : string {
-            return $this->name;
-
-        }
-
-        public function getUnitPrice() : float {
-            return $this->unit_price;
-
-        }
-
-        public function getQuantity() : int {
-            return $this->quantity;
-
-        }
+    public function __construct(string $name, float $unit_price, int $quantity)
+    {
+        $this->name = $name;
+        $this->unit_price = $unit_price;
+        $this->quantity = $quantity;
 
     }
+
+    public function getName() : string {
+        return $this->name;
+
+    }
+
+    public function getUnitPrice() : float {
+        return $this->unit_price;
+
+    }
+
+    public function getQuantity() : int {
+        return $this->quantity;
+
+    }
+
+}
---
+++ result.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos</title>
+</head>
+<body>
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            require_once './Classes/Product.php';
+        
+            if (isset($_POST['register'])) {
+                $name = $_POST['name'];
+                $unitPrice = $_POST['unit-price'];
+                $qty = $_POST['qty'];
+
+                $product = new Product($name, $unitPrice, $qty);
+
+                echo "Produto: {$product->getName()}, registrado com sucesso! <br>";
+
+                if ($product->getQuantity() < 5 ) {
+                    echo "Alerta vermelho! Produto com estoque crítico! Estoque: {$product->getQuantity()}";
+                }
+                else if ($product->getQuantity() < 10) {
+                    echo "Alerta! Produto com estoque baixo! Estoque: {$product->getQuantity()}";
+                }
+                else {
+                    echo "Estoque saudável!";
+                }
+
+            }
+        }
+    ?>
+
+    <br>
+    <a href="index.php">Voltar</a>
+    
+</body>
+</html>
