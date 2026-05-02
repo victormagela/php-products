f3644737445d265dab8f165c3fdf5979204eadaa
Victor Magela <victormateusamato@outlook.com>
2026-05-02 15:26:13 -0300

    Write the CSS for the register page

--- index.php
+++ index.php
@@ -3,30 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Cadastro de Produtos</title>
 </head>
 <body>
-    <h1>Cadastre seu produto</h1>
-
-    <form action="./result.php" method="post">
-        <div class="form-field">
-            <label for="name">Nome:</label>
-            <input type="text" name="name" id="name">
-        </div>
-        
-        <div class="form-field">
-            <label for="unit-price">Preço unitário:</label>
-            <input type="number" name="unit-price" id="unit_price" step="0.01">
-        </div>
-        
-        <div class="form-field">
-            <label for="qty">Quantidade:</label>
-            <input type="number" name="qty" id="qty">
-        </div>
-
-        <input type="submit" value="Cadastrar" name="register">
+    <div class="container">
+        <h1>Cadastre seu produto</h1>
     
-    </form>
+        <form action="./result.php" method="post">
+            <div class="form-field">
+                <label for="name">Nome:</label>
+                <input type="text" name="name" id="name" required>
+            </div>
+            
+            <div class="form-field">
+                <label for="unit-price">Preço unitário:</label>
+                <input type="number" name="unit-price" id="unit-price" step="0.01" required>
+            </div>
+            
+            <div class="form-field">
+                <label for="qty">Quantidade:</label>
+                <input type="number" name="qty" id="qty" required>
+            </div>
+    
+            <input type="submit" value="Cadastrar" name="register">
+        
+        </form>
+
+    </div>
     
 </body>
 </html>
